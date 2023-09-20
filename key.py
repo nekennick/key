e6b249830f77a75d9bbfea484fe3b5bdfe116580
Khoa <nekennick@gmail.com>
2023-09-20 22:07:47 +0700

    nice

--- key.py
+++ key.py
@@ -6,13 +6,21 @@ def on_click(x, y, button, pressed):
     if pressed and button == button.left:
         selected_text = pyperclip.paste()
         selected_text_label.config(text=f'Văn bản được chọn: {selected_text}')
-        app.update()  # Cập nhật giao diện
+        compare_text(selected_text)
+
+def compare_text(selected_text):
+    with open('text.txt', 'r', encoding='utf-8') as file:  # Chắc chắn sử dụng mã hóa utf-8
+        data = file.read()
+        if selected_text in data:
+            result_label.config(text='Kết quả: yes')
+        else:
+            result_label.config(text='Kết quả: no')
 
 app = tk.Tk()
-app.title('Ứng dụng hiển thị văn bản được chọn')
+app.title('Ứng dụng so sánh văn bản được chọn')
 
 # Đặt kích thước giao diện
-app.geometry('500x50')
+app.geometry('500x100')
 
 # Đặt cửa sổ luôn xuất hiện trên cùng
 app.attributes('-topmost', True)
@@ -20,6 +28,9 @@ app.attributes('-topmost', True)
 selected_text_label = tk.Label(app, text='')
 selected_text_label.pack()
 
-# Bắt đầu theo dõi sự kiện chuột để lấy văn bản đã chọn
+result_label = tk.Label(app, text='')
+result_label.pack()
+
+# Bắt đầu theo dõi sự kiện chuột để lấy và so sánh văn bản
 with Listener(on_click=on_click) as listener:
     app.mainloop()
