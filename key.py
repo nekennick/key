59cead197c031c720c6500690c2508b48a271893
Khoa <nekennick@gmail.com>
2023-09-25 22:13:03 +0700

    save

--- key.py
+++ key.py
@@ -1,12 +1,18 @@
 import tkinter as tk
 import pyperclip
 from pynput.mouse import Listener
+import keyboard
 
 def on_click(x, y, button, pressed):
     if pressed and button == button.left:
         selected_text = pyperclip.paste()
-        selected_text_label.config(text=f'Văn bản được chọn: {selected_text}')
+        selected_text_label.config(text=f'Selected: {truncate_text(selected_text)}')
         compare_text(selected_text)
+
+def truncate_text(text, max_length=30):
+    if len(text) > max_length:
+        return text[:max_length] + '...'
+    return text
 
 def compare_text(selected_text):
     found = False
@@ -14,27 +20,41 @@ def compare_text(selected_text):
         for line in file:
             parts = line.split('|')
             if len(parts) > 1 and selected_text.strip() == parts[0].strip():
-                result_label.config(text=f'Kết quả: {parts[1].strip()}')
+                result_var.set(f'KQ: {parts[1].strip()}')
                 found = True
                 break
     if not found:
-        result_label.config(text='Kết quả: Không tìm thấy')
+        result_var.set('KQ: không thấy')
+
+def close_app():
+    app.destroy()
 
 app = tk.Tk()
-app.title('Ứng dụng so sánh văn bản được chọn')
-
-# Đặt kích thước giao diện
-app.geometry('500x100')
+app.title('Bypass Elearning')
 
 # Đặt cửa sổ luôn xuất hiện trên cùng
 app.attributes('-topmost', True)
 
-selected_text_label = tk.Label(app, text='')
+# Ẩn nút close, minimize và maximize
+app.overrideredirect(True)
+
+# Đặt kích thước giao diện và vị trí góc dưới bên trái
+app.geometry('150x50+0+{}' .format(app.winfo_screenheight() - 60))
+
+frame = tk.Frame(app)
+frame.pack(expand=True, fill="both")
+
+selected_text_label = tk.Label(frame, text='')
 selected_text_label.pack()
 
-result_label = tk.Label(app, text='')
-result_label.pack()
+result_var = tk.StringVar()
+result_label = tk.Label(frame, textvariable=result_var, font=('Helvetica', 14), justify="center")
+result_label.pack(expand=True)
 
 # Bắt đầu theo dõi sự kiện chuột để lấy và so sánh văn bản
 with Listener(on_click=on_click) as listener:
+    # Thêm sự kiện theo dõi phím Ctrl + E để đóng cửa sổ
+    keyboard.add_hotkey('ctrl+e', close_app)
+    app.protocol("WM_DELETE_WINDOW", close_app)  # Gắn sự kiện đóng cửa sổ
+
     app.mainloop()
